106e43f4816d6ae23b5f60fbae0a118a087f17f5
edgarriba <edgar.riba@gmail.com>
2015-11-02 11:42:09 +0100

    add mouse callback

---
+++ build/CMakeFiles/gdal-image-io.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gdal-image-io.dir/gdal-image-io.cpp.o"
+  "gdal-image-io.pdb"
+  "gdal-image-io"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gdal-image-io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
